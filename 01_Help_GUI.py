ae15fbf76624a2761e442482b7e08c6c04bef02a
kuschelyoungl71929 <99296445+kuschelyoungl71929@users.noreply.github.com>
2022-02-11 10:00:16 +1300

    Update 01_Help_GUI.py

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -7,9 +7,12 @@ class Converter:
 
         bkg_colour = "#9fe5d9"
 
+ 
         self.converter_frame = Frame(width=400, height=300, bg=bkg_colour)
         self.converter_frame.grid()
 
+    self.temp_converter_label = Label(text="Temperature Converter" font=("Arial", "18", "bold"), bg=bkg_colour padx=10, pady=10)
+    self.temp_converter_label.grid(row=0)
 
 # main routine
 if __name__ == "__main__":
